4ea7e1e79b4d5e635bd222ff9ea9c9a1eca515f3
Kevin <playsbykevin@gmail.com>
2023-09-21 21:16:00 +0300

    sidebar added

--- semu-frontend/src/app/app-routing.module.ts
+++ semu-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./pages/main/main.component";
+import {LogInComponent} from "./pages/log-in/log-in.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'log-in', component: LogInComponent},
+  {path: 'home', component: MainComponent},
+  {path: '', redirectTo: '/log-in', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
