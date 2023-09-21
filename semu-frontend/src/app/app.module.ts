4a07877c844e2fe184ea9a95f76310631a8e347b
Kevin <playsbykevin@gmail.com>
2023-09-21 21:16:00 +0300

    sidebar added

--- semu-frontend/src/app/app.module.ts
+++ semu-frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './pages/log-in/log-in.component';
+import { MainComponent } from './pages/main/main.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponent
+    LogInComponent,
+    MainComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
