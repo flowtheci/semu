e61ea1769d8ea0d7a4229eef4a113cacceb30b75
Kevin <playsbykevin@gmail.com>
2023-09-30 05:28:53 +0300

    ok idk dependency fix fuck backend development

--- api/src/main/java/com/semu/api/controller/UserController.java
+++ api/src/main/java/com/semu/api/controller/UserController.java
@@ -4,12 +4,14 @@ import com.semu.api.model.User;
 import com.semu.api.service.UserService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @RestController
@@ -25,6 +27,7 @@ public class UserController {
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
         User registeredUser = userService.registerUser(user);
+        System.out.println(Base64.encodeBase64String(jwtSecret.getBytes()));
         String token = Jwts.builder()
                 .setSubject(registeredUser.getEmail())
                 .setExpiration(new Date(System.currentTimeMillis() + 864000000)) // 1 day expiration
