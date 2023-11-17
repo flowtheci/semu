22ebf0970892027cce177f6f049ff7a9be963d13
Kevin <playsbykevin@gmail.com>
2023-11-18 01:17:54 +0200

    pls fkin toota

--- semu-frontend/src/app/app.component.ts
+++ semu-frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ViewportScroller} from "@angular/common";
+import {NavigationEnd, Router} from "@angular/router";
 
 
 export const devMode = false;
@@ -12,4 +14,12 @@ export const backendUrl = devMode ? 'http://localhost:8080/api/' : 'https://trib
 
 export class AppComponent {
   title = 'SEMU';
+
+  constructor(private router: Router, private viewportScroller: ViewportScroller) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.viewportScroller.scrollToPosition([0,0]);
+      }
+    });
+  }
 }
